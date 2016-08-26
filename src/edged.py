af63281cade4b8884ac9a309c4121518ed4a6441
Seth Voltz <seth@designgods.net>
2016-08-26 15:18:24 -0700

    Completed command router and stubbed in commands

--- src/edged.py
+++ src/edged.py
@@ -4,12 +4,12 @@ import json
 
 class EdgeProtocol(basic.LineReceiver):
     def lineReceived(self, line):
-        deferred = self.factory.parseCommand(line)
+        deferred = self.factory.parse_command(line)
 
-        def writeResponse(message):
+        def writeResponse(data):
             self.transport.write(json.dumps({
                 'success': True,
-                'message': message
+                'data': data
                 }) + "\r\n")
             self.transport.loseConnection()
         deferred.addCallback(writeResponse)
@@ -26,14 +26,55 @@ class EdgeProtocol(basic.LineReceiver):
 class EdgeFactory(protocol.Factory):
     protocol = EdgeProtocol
 
-    def parseCommand(self, line):
-        "setColors, getColors, getLock, releaseLock, setMode, getMode"
+    def __init__(self):
+        self.dispatch = {
+            'setColors': self.set_colors,
+            'getColors': self.get_colors
+        }
+
+    def parse_command(self, line):
+        "Parse the incomming command and route to correct handler"
+
         try:
             data = json.loads(line)
-        except (ValueError):
-            return defer.fail()
+        except:
+            return defer.fail(Exception('Unable to parse JSON payload'))
 
-        return defer.succeed(data)
+        if 'command' not in data or data['command'] not in self.dispatch:
+            return defer.fail(Exception('Unknown or missing command'))
+
+        return self.dispatch[data['command']](data)
+
+    def set_colors(self, command):
+        deferred = defer.Deferred()
+        deferred.callback({ 'colors': [1, 2, 3, 4, 5, 6] })
+        return deferred
+
+    def get_colors(self, command):
+        deferred = defer.Deferred()
+        deferred.errback(Exception('Not Implemented'))
+        return deferred
+
+    def request_lock(self, command):
+        deferred = defer.Deferred()
+        deferred.errback(Exception('Not Implemented'))
+        return deferred
+
+    def release_lock(self, command):
+        deferred = defer.Deferred()
+        deferred.errback(Exception('Not Implemented'))
+        return deferred
+
+    def set_mode(self, command):
+        deferred = defer.Deferred()
+        deferred.errback(Exception('Not Implemented'))
+        return deferred
+
+    def get_mode(self, command):
+        deferred = defer.Deferred()
+        deferred.errback(Exception('Not Implemented'))
+        return deferred
+
 
 def main():
     endpoints.serverFromString(reactor, "tcp:1234").listen(EdgeFactory())
